2cd33bfe4405da4dd4cc40a16a8a8892a4624f5d
João Duarte <jpdx2010@hotmail.com>
2024-01-17 15:46:18 -0300

    fix

--- projeto-dois/src/app/meu-primeiro2/meu-primeiro2.component.ts
+++ projeto-dois/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-meu-primeiro2',
+  
   // standalone: true,
   // imports: [],
   templateUrl: './meu-primeiro2.component.html',
