6c0041aeed0e180a1262a0de5ef02c9cb6c9a6af
João Duarte <jpdx2010@hotmail.com>
2024-02-27 21:50:33 -0300

    refresh

--- projeto-dois/src/app/app.component.ts
+++ projeto-dois/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Cursos1Module } from "./cursos1/cursos1.module";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  // imports: [CommonModule, RouterOutlet],
-  template: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    // imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [Cursos1Module]
 })
 export class AppComponent {
-  title = 'Olá Mundo';
+  title = 'João';
 }
